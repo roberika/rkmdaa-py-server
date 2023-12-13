3d2e795318c3d619381d0cef9349445cc868206f
Robert Antonius <robert.antonius23@gmail.com>
2023-12-14 00:43:52 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -15,61 +15,51 @@ def about():
     return 'About'
 
 def load_model():
-    if "anime_recommendation_model" not in globals():
-        global anime_recommendation_model
-        anime_recommendation_model = pickle.load(open('data/anime_recommendation_model.sav', 'rb'))
+    return pickle.load(open('data/anime_recommendation_model.sav', 'rb'))
 
 def load_user_list(username):
-    if "user_history" not in globals() and "user_scores" not in globals() :
-        global user_history
-        global user_scores
-        item_per_page = 1000
-        i = 1
-        url = "https://api.myanimelist.net/v2/users/"+str(username)+"/animelist?fields=node,list_status&limit=" + str(item_per_page)
-        headers = {'X-MAL-CLIENT-ID': 'fe954b2111fb0add5b5b25bfaa638979', 'Accept': '*/*'}
-        response = rq.get(url, headers=headers)
-        user = response.json()["data"]
-        while (len(response.json()["paging"]) != 0 and len(response.json()["paging"]["next"]) != 0):
-            response = rq.get(url + "&offset=" + str(item_per_page * i), headers=headers)
-            user = user | response.json()["data"]
-            i += 1
-        ids = [anime["node"]["id"] for anime in user]
-        scores = [anime["list_status"]["score"] for anime in user]
-        list_anime = {"anime_id": ids, "rating": scores}
-        user_history = pd.DataFrame({"anime_id": ids, "rating": scores})
-        user_history = user_history.loc[user_history.anime_id <= 48492]
-        user_scores = user_history.loc[user_history.rating != 0]
+    item_per_page = 1000
+    i = 1
+    url = "https://api.myanimelist.net/v2/users/"+str(username)+"/animelist?fields=node,list_status&limit=" + str(item_per_page)
+    headers = {'X-MAL-CLIENT-ID': 'fe954b2111fb0add5b5b25bfaa638979', 'Accept': '*/*'}
+    response = rq.get(url, headers=headers)
+    user = response.json()["data"]
+    while (len(response.json()["paging"]) != 0 and len(response.json()["paging"]["next"]) != 0):
+        response = rq.get(url + "&offset=" + str(item_per_page * i), headers=headers)
+        user = user | response.json()["data"]
+        i += 1
+    ids = [anime["node"]["id"] for anime in user]
+    scores = [anime["list_status"]["score"] for anime in user]
+    list_anime = {"anime_id": ids, "rating": scores}
+    user_history = pd.DataFrame({"anime_id": ids, "rating": scores})
+    user_history = user_history.loc[user_history.anime_id <= 48492]
+    user_scores = user_history.loc[user_history.rating != 0]
+    return user_history, user_scores
 
-def get_recommendation(anime_id):
-    load_model()
-    return anime_recommendation_model.loc[anime_recommendation_model.MAL_ID == anime_id].values.flatten()[:-1]
+def append_predicted_scores(anime_list, user_scores):
+    return [[rec, user_scores.loc[user_scores.anime_id.isin(get_recommendation(rec))].rating.mean()] for rec in anime_list]
 
-def get_recommendations(anime_ids):
-    load_model()
+@app.route('/recommend/<username>/<id>')
+def get_recommendation_with_scores(username, id):
+    user_history, user_scores = load_user_list(username)
+    anime_recommendation_model = load_model()
+    
+    recs = anime_recommendation_model.loc[anime_recommendation_model.MAL_ID == id].values.flatten()[:-1]
+    
+    return append_predicted_scores(recs, user_scores)
+
+@app.route('/recommend/<username>')
+def get_recommendations_for_current_user_with_scores(username):
+    user_history, user_scores = load_user_list(username)
+    anime_recommendation_model = load_model()
+    
+    anime_ids = user_history.anime_id.to_list()
     recs = anime_recommendation_model.loc[anime_recommendation_model.MAL_ID.isin(anime_ids)].values.flatten()
     recs = [i for i in recs if i not in anime_ids]
     recs_sorted, indices = np.unique(np.array(recs), return_index=True)
-    return [recs[index] for index in sorted(indices)]
-
-def append_predicted_scores(anime_list):
-    load_user_list(username)
-    return [[rec, user_scores.loc[user_scores.anime_id.isin(get_recommendation(rec))].rating.mean()] for rec in anime_list]
-
-@app.route('/get_recommendation/<id>')
-def get_recommendation_with_scores(id):
-    return append_predicted_scores(get_recommendation(id))
-
-def get_recommendations_with_scores(anime_ids):
-    return append_predicted_scores(get_recommendations(anime_ids))
-
-def get_recommendations_for_current_user(username):
-    load_user_list(username)
-    return get_recommendations(user_history.anime_id.to_list())
-
-@app.route('/get_recommendations/<username>')
-def get_recommendations_for_current_user_with_scores(username):
-    load_user_list(username)
-    return get_recommendations_with_scores(user_history.anime_id.to_list())
+    recs = [recs[index] for index in sorted(indices)]
+    
+    return append_predicted_scores(recs, user_scores)
 
 
     
