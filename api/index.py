3de6a173ecdfbaaa9bda5b5c8235123818b2b648
Robert Antonius <robert.antonius23@gmail.com>
2023-12-14 14:58:53 +0700

    Added return user history

--- api/index.py
+++ api/index.py
@@ -63,5 +63,11 @@ def get_recommendations_for_current_user_with_scores(username):
     
     return append_predicted_scores(recs, user_scores)
 
+@app.rout('/history/<username>')
+def get_user_history_with_scores(username):
+    user_history, user_scores = load_user_list(username)
+    return [[str(anime.anime_id), str(anime.rating)] for anime in user_history.values]
+    
+    
 
     
